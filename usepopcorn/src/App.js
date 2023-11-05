a503d939c994e8ae0368392b866ad52f054f8d94
raihankabir36850 <raihanku320@gmail.com>
2023-11-06 01:27:06 +0600

    for testing to see how it works

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -11,6 +11,7 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
   return (
     <>
       <NavComponent>
