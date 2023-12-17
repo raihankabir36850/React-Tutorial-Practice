2a7f7a78dda9dd1d9fe428398e75da6ed2981b17
raihankabir36850 <raihanku320@gmail.com>
2023-12-17 17:53:06 +0600

    practice: components with different props

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Demo from './Test/Demo';
+import Demo2 from './Test/Demo2';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Demo2 />
   </React.StrictMode>
 );
 
