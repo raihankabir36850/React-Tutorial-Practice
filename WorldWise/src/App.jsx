7951cb9ae7ab5819dd9bb43a24c835e77d6f45bd
raihankabir36850 <raihanku320@gmail.com>
2024-01-14 19:55:15 +0600

    build up the structure and wrap up with simple routing of the new project named worldwise

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+
 function App() {
-  return <>World Wise</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
